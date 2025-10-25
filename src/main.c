df13a5a12ff7387db41f4e10a08e62d5fc9c85d6
eric-albuquer <ega2@cesar.school>
2025-10-25 11:46:35 -0300

    Adicionando pasta dos assets

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(void) {
     SetTargetFPS(60);
     
     Render* render = new_Render(1920, 1080, 50, 30);
-    Map* map = new_Map(1000, 1000);
+    Map* map = new_Map(1000, 1000, 30);
 
     while (!WindowShouldClose()) {
         map->update(map);
