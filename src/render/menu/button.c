7c7ad4e3f1ce29ba6f78f352cccab81329fbf92a
Ricardo Freitas <ricardo.sergio.pff@gmail.com>
2025-11-11 20:53:09 -0300

    adicionei umas chamas no menu

--- src/render/menu/button.c
+++ src/render/menu/button.c
@@ -10,12 +10,19 @@ bool isInside(Button* this, Vector2 pos){
     return pos.x > this->x && pos.x < this->x + this->width && pos.y > this->y && pos.y < this->y + this->height;
 }
 
-void drawButton(Button* this){
-    DrawRectangle(this->x, this->y, this->width, this->height, this->color);
-    int halfX = this->x + (this->width >> 1);
-    int halfY = this->y + (this->height >> 1);
-    DrawText(this->text, halfX, halfY, this->fontSize, this->fontColor);
+void drawButton(Button* this) {
+    Color color = this->hovered ? this->fontColor : this->color;
+    DrawRectangle(this->x, this->y, this->width, this->height, color);
+
+    int textWidth = MeasureText(this->text, this->fontSize);
+    int textX = this->x + (this->width - textWidth) / 2;
+    int textY = this->y + (this->height - this->fontSize) / 2;
+
+    Color textColor = this->hovered ? WHITE : this->fontColor;
+
+    DrawText(this->text, textX, textY, this->fontSize, textColor);
 }
+
 
 Button* new_Button(int x, int y, int w, int h, Color color, Color fontColor, char* text, int fontSize, void (*action)()){
     Button* this = malloc(sizeof(Button));
--- src/render/menu/menu.c
+++ src/render/menu/menu.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 #include "common.h" 
-#include <raylib.h>  
+#include <raylib.h> 
+#include <math.h>
 
 
 // ---- Aqui sao os botoes ---- 
@@ -39,13 +40,68 @@ void _free(Menu* this){
     free(this);
 }
 
+static void ResetFlame(FlameParticle* f, int screenWidth, int screenHeight) {
+    f->x = (float)(rand() % screenWidth);
+    f->y = (float)(screenHeight + rand() % 100);   
+    f->speed = 40.0f + (float)(rand() % 80);       
+    f->radius = 2.0f + (float)(rand() % 4);        
+
+    unsigned char r = 255;
+    unsigned char g = 150 + rand() % 80;
+    unsigned char b = 0;
+    unsigned char a = 150 + rand() % 105;
+    f->color = (Color){ r, g, b, a };
+}
+
+
 void draw(Menu* this){
     if (!this) return;
 
-    if (this->play)      this->play->draw(this->play);
-    if (this->volume)    this->volume->draw(this->volume);
-    if (this->difficulty)this->difficulty->draw(this->difficulty);
+    float t = GetTime();
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
+
+    Color topColor = (Color){ (int)(40 + 20*sin(t*0.5)), 0, 0, 255 };
+    Color bottomColor = (Color){ (int)(150 + 80*sin(t*0.3)), 0, 0, 255 };
+
+    for (int y = 0; y < screenHeight; y++) {
+        float factor = (float)y / (float)screenHeight;
+        Color c = {
+            (unsigned char)(topColor.r*(1.0f - factor) + bottomColor.r*factor),
+            (unsigned char)(topColor.g*(1.0f - factor) + bottomColor.g*factor),
+            (unsigned char)(topColor.b*(1.0f - factor) + bottomColor.b*factor),
+            255
+        };
+        DrawLine(0, y, screenWidth, y, c);
+    }
+
+    if (!flamesInit) {
+        for (int i = 0; i < NUM_FLAMES; i++) {
+            ResetFlame(&flames[i], screenWidth, screenHeight);
+            flames[i].y = (float)(rand() % screenHeight);
+        }
+        flamesInit = true;
+    }
+
+    float dt = GetFrameTime();
+    for (int i = 0; i < NUM_FLAMES; i++) {
+        FlameParticle* f = &flames[i];
+
+        f->y -= f->speed * dt;
+
+        if (f->y + f->radius < 0) {
+            ResetFlame(f, screenWidth, screenHeight);
+        }
+
+        DrawCircle((int)f->x, (int)f->y, f->radius, f->color);
+    }
+
+    if (this->play)       this->play->draw(this->play);
+    if (this->volume)     this->volume->draw(this->volume);
+    if (this->difficulty) this->difficulty->draw(this->difficulty);
 }
+
+
 
 void update(Menu* this){
     if (!this) return;
